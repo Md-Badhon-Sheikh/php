38368eb70fc2eb7cf99ee5c59f0f811df4b57fe8
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-12-08 13:00:35 +0600

    added files

---
+++ insert-update-delete-with-mysql/display.php
@@ -0,0 +1,69 @@
+<?php
+    // include connection file 
+    require_once 'conn.php';
+
+    // delete query
+    if(isset($_GET['deleteId'])){
+        $delete = $_GET['deleteId'];
+
+        $sql = "DELETE FROM trainee_info WHERE id = $delete";
+        if(mysqli_query($conn,$sql) == TRUE){
+            header('location:display.php');
+        }
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Table</title>
+    <link rel="stylesheet" href="styles/display.css">
+</head>
+<body>
+    <div class="table-container">
+        <h2>Data Table</h2>
+        <table class="data-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th colspan="2">Action</th>
+                </tr>
+            </thead>
+            <?php
+            if(!$conn){
+                die('Connection failed:'.mysqli_connect_error());
+            }
+            else{
+                $display = $conn-> query("select * from trainee_info");
+                $count = 1;
+                while(list($id,$name,$email,$phone) = $display->fetch_row()){
+                    $id = $count++;
+                    echo " 
+                        <tbody>
+                <tr>
+                    <td>$id</td>
+                    <td>$name</td>
+                    <td>$email</td>
+                    <td>$phone</td>
+                    <td><button class='edit-btn'><a href='edit.php?editId=$id'>Edit</a></button></td>
+                    <td><button class='action-btn'><a href='display.php?deleteId=$id'>Delete</a></button></td>
+                </tr>
+            </tbody>";
+                }
+            }
+            ?>
+        
+        </table>
+        <button><a href="insert.php">Back to insert</a></button>
+    </div><br>
+
+</body>
+</html>
+
