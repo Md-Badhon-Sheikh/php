4965634c06963ba9f8d9372829d1d925cf1c8e31
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-12-14 12:32:07 +0600

    added files

--- products/product_form.php
+++ products/product_form.php
@@ -1,11 +1,18 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'product');
+$conn = new mysqli('localhost', 'root', '', 'product');
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $contact = $_POST['contact'];
 
     $conn->query("call  product_routine('$name','$contact')");
+}
+if (isset($_POST['addProduct'])) {
+    $pname = $_POST['pname'];
+    $price = $_POST['price'];
+    $manuf = $_POST['manuf'];
+
+    $conn->query("call manufact('$pname', '$manuf','$price')");
 }
 
 ?>
@@ -17,56 +24,96 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product</title>
+    <link rel="stylesheet" href="styles/style.css">
 
 </head>
 
 <body>
+    <!-- Brand added  -->
     <div class="login-container">
-
-        <form action="" method="post">
-            <div class="form-group">
+        <h2>Add Brand</h2>
+        <form action="#" method="POST">
+            <div class="input-group">
                 <label for="username">Brand:</label>
-                <input type="text" id="name" name="name" required>
+                <input type="text" id="username" name="name" required>
             </div>
-            <div class="form-group">
+            <div class="input-group">
                 <label for="password">Contact</label>
-                <input type="text" id="" name="contact" required>
+                <input type="text" id="password" name="contact" required>
             </div>
-            <div class="form-group">
-                <button type="submit" name="submit">Submit</button>
-            </div>
-
+            <button type="submit" name="submit">Submit</button>
         </form>
+    </div>
+
     </div> <br><br>
 
-
+<!-- Product  table -->
     <div class="login-container">
-
-        <form action="/login" method="post">
+    <h2>Product from</h2>
+        <form action="" method="post">
             <div class="form-group">
                 <label for="username">Product Name:</label>
                 <input type="text" id="name" name="pname" required>
             </div>
             <div class="form-group">
                 <label for="price">Product Price:</label>
-                <input type="text"name="price" required>
+                <input type="text" name="price" required>
             </div>
             <div class="form-group">
                 <label for="price">Manufacture:</label>
-               <select name="manufact" id="">
+                <select name="manuf" id="" class="options">
                     <?php
-                        $manufact = $conn->query('select * from brand');
-                        while(list($_mid,$_mname) = $manufact->fetch_row()){
-                            echo "<option value ='$_mid'$_mname</option> ";
-                        }
+                    $manufactList = $conn->query('select * from brand');
+                    while (list($_mid, $_mname) = $manufactList->fetch_row()) {
+                        echo "<option value ='$_mid'>$_mname</option> ";
+                    }
                     ?>
-               </select>
+                </select>
             </div>
             <div class="form-group">
                 <button type="submit" name="addProduct">AddProduct</button>
             </div>
 
         </form>
+    </div><br><br>
+
+    <!-- data view  -->
+
+    <div class="table-container">
+        <h2>Data Table</h2>
+        <table class="data-table">
+            <thead>
+                <tr>
+                    <th>SL</th>
+                    <th>Brand</th>
+                    <th>Contact</th>
+                    <th>Product Name</th>
+                    <th>Product Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $conn = mysqli_connect('localhost', 'root', '', 'product');
+                if (!$conn) {
+                    die('Connection failed:' . mysqli_connect_error());
+                } else {
+                    $user = $conn->query("SELECT * FROM product_view");
+                    $counter = 1;
+                    while (list($brand, $contact, $product_name, $price) = $user->fetch_row()) {
+                        $sl=$counter++;
+                        echo "<tr>
+                    <td>$sl</td>
+                    <td>$brand</td>
+                    <td>$contact</td>
+                    <td>$product_name</td>
+                    <td>$price</td>
+                </tr>";
+                    }
+                }
+                ?>
+
+            </tbody>
+        </table>
     </div>
 </body>
 
